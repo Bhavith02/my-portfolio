306efba481539d40ef83a17f5544f63080bbf386
Bhavith02 <bhavith070202@gmail.com.com>
2023-02-13 15:15:26 +0530

    project changes 2 done

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,26 +4,21 @@ const About = () => {
   return (
     <div name='about' className='w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white'>
         <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
-            <div className='pb-8'>
+            <div className=' pb-6'>
                 <p className='text-4xl font-bold inline border-b-4 border-gray-500'>About</p>
             </div>
             <p className='text-xl mt-20'>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius,
-          deserunt illum mollitia officiis qui exercitationem perferendis neque
-          quasi a recusandae necessitatibus tempora iusto! Blanditiis error
-          iste, totam fugiat recusandae rerum laborum perferendis molestiae
-          aperiam asperiores nemo. Magni dolor maxime debitis vitae, eaque hic
-          ab mollitia voluptatibus, a nostrum eveniet laborum!
-          </p>
+                Hello there! My name is S Bhavith. I'am from Bangalore. Currently
+                studying Bachelor's of Engineering in the field of computer science 
+                from JSS Academy of Technical Education Bangalore with a CGPA just over 9. 
+                I have a strong passion for development. I've recently worked as a software 
+                intern developer in a start up called <a href="https://RenderPub.com/" className='text-cyan-500' target='_blank' rel='noreferrer' >RenderPub</a>, where 
+                I worked using svelte technology to design their website.     
+            </p>
             <br/>
             <p className='text-xl'>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius,
-          deserunt illum mollitia officiis qui exercitationem perferendis neque
-          quasi a recusandae necessitatibus tempora iusto! Blanditiis error
-          iste, totam fugiat recusandae rerum laborum perferendis molestiae
-          aperiam asperiores nemo. Magni dolor maxime debitis vitae, eaque hic
-          ab mollitia voluptatibus, a nostrum eveniet laborum!
-          </p>
+
+            </p>
         </div>
     </div>
   )
